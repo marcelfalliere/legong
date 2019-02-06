e06e8d3bde7dcac8f65c07dfc2dd50120fd0a969
Frédéric Falliere <frederic.falliere@gmail.com>
2019-02-06 16:51:41 +0100

    yo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import css from './index.css';
-import flexboxgrid from 'flexboxgrid';
+import css from './styles/index.css';
+import simpleGrid from './styles/libs/simple-grid.css';
 
 import main from './src/main';
 
