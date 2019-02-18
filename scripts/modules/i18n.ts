0229cf4d2960bc26733ec8220e781cccb5aa9e99
Frederic Falliere <frederic.falliere@gmail.com>
2019-02-18 08:07:08 +0100

    preparing i18n and advanced responsive

--- scripts/modules/i18n.ts
+++ scripts/modules/i18n.ts
@@ -1,8 +1,51 @@
 
 export class i18n {
+
+    static AVAILABLE_LANGUAGES = [
+        {
+            label: 'Français',
+            prefix: 'fr'
+        }
+    ]
+
+    static DEFAULT_LANGUAGE = 'en';
     
     public static bootstrap() :void {
-        console.log('a');
+        
+        //const lang = i18n.detectLanguage();
+        const lang = 'fr';
+
+        console.log(`[i18n] detected ${lang}`);
+
+        if (lang !== i18n.DEFAULT_LANGUAGE) {
+            i18n.loadUiForLanguage(lang);
+        }
+
+    }
+
+    private static loadUiForLanguage(lang: string) {
+        console.log(`[i18n] loadUiForLanguage for ${lang}`);
+    }
+
+    private static detectLanguage(): string {
+    
+        let found = i18n.DEFAULT_LANGUAGE;
+        
+        if (typeof window.navigator !== 'undefined') {
+            if (window.navigator.languages && window.navigator.languages.length > 0) {
+                found = window.navigator.languages[0];
+            } else if (window.navigator.userLanguage) {
+                found = window.navigator.userLanguage;
+            } else if (window.navigator.language) {
+                found = window.navigator.language;
+            }
+        }
+
+        if (found.indexOf('-')) {
+            found = found.substring(0, found.indexOf('-'));
+        }
+    
+        return found;
     }
 
 }
