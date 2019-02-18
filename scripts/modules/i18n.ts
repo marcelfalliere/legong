f7f39e428b5ea283215f612e509e2c23b71aa8a2
Frédéric Falliere <frederic.falliere@gmail.com>
2019-02-18 10:02:27 +0100

    some more i18n stuff

--- scripts/modules/i18n.ts
+++ scripts/modules/i18n.ts
@@ -5,26 +5,56 @@ export class i18n {
         {
             label: 'Français',
             prefix: 'fr'
+        }, {
+            label: 'English',
+            prefix: 'en'
         }
     ]
 
     static DEFAULT_LANGUAGE = 'en';
-    
+
     public static bootstrap() :void {
         
-        //const lang = i18n.detectLanguage();
-        const lang = 'fr';
+        const lang = i18n.detectLanguage();
+        
+        const langExists = i18n.AVAILABLE_LANGUAGES.find(l => l.prefix === lang);
+        
+        if (!langExists) {
+            i18n.initUiForLanguage(i18n.DEFAULT_LANGUAGE);
+        } else {
+            i18n.initUiForLanguage(lang);
+        }
+        
+    }
+    
+    private static initUiForLanguage(lang: string) {
+        console.log(`[i18n] initUiForLanguage for ${lang}`);
+        
+        const $ui = document.querySelector('#i18n-menu');
+        
+        $ui.classList.remove('hidden');
 
-        console.log(`[i18n] detected ${lang}`);
+        const $select = $ui.querySelector('select');
+
+        $select.value = lang;
 
         if (lang !== i18n.DEFAULT_LANGUAGE) {
-            i18n.loadUiForLanguage(lang);
+            i18n.loadLangStrings(lang);
         }
+
+        $select.addEventListener('change', (event: Event) => {
+            const lang = (event.target as HTMLSelectElement).value;
+
+            i18n.loadLangStrings(lang);
+        });
 
     }
 
-    private static loadUiForLanguage(lang: string) {
-        console.log(`[i18n] loadUiForLanguage for ${lang}`);
+    private static loadLangStrings(lang: string) {
+        // TODO ; mieux d'avoir une page index.html par langue ? de toute façon il n'y aura qu'une page ... ou alors de faire du JS pour internationnlisé ?
+        // https://webmasters.stackexchange.com/questions/119138/root-index-html-file-of-a-multi-language-website
+        // et balises meta
+        // <meta http-equiv=”content-language” content=”en-us”>
     }
 
     private static detectLanguage(): string {
